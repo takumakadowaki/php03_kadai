fa60c282af6a0b17855867d1be2ae369b354b19d
takumakadowaki <149216447+takumakadowaki@users.noreply.github.com>
2024-02-09 08:22:36 +0900

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,34 @@
+<?php
+
+//1.対象のIDを取得
+// GETで取得するので、GETに書き換え
+$id   = $_GET['id'];
+
+//2.DB接続します
+try {
+    $db_name = 'gs_db3'; //データベース名
+    $db_id   = 'root'; //アカウント名
+    $db_pw   = ''; //パスワード：MAMPは'root'
+    $db_host = 'localhost'; //DBホスト
+    $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
+} catch (PDOException $e) {
+    exit('DB Connection Error:' . $e->getMessage());
+}
+
+
+//3.削除SQLを作成
+$stmt = $pdo->prepare('DELETE FROM gs_an_table WHERE id = :id');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$status = $stmt->execute(); //実行
+
+
+//４．データ登録処理後
+if ($status === false) {
+    //*** function化する！******\
+    $error = $stmt->errorInfo();
+    exit('SQLError:' . print_r($error, true));
+} else {
+    //*** function化する！*****************
+    header('Location: select.php');
+    exit();
+}
